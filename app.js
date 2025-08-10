12656497defeca0f376b72d549c60e7b2692dad1
ohmyraffle <sophiejanetyrersales@gmail.com>
2025-08-10 20:43:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 // You can add interactive features here later
 console.log("Oh My Raffle site loaded!");
+// Register service worker for PWA
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/oh-my-raffle/sw.js', { scope: '/oh-my-raffle/' })
+    .catch(err => console.log('SW registration failed:', err));
+}
